c75ed01f3ed6ede37c0ba04166966b248e62f262
Tom Brus <tombrus@gmail.com>
2019-11-25 00:04:36 +0100

    initial push

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,14 @@ includeBuildTools() {
 
   local buildToolsUrl="https://maven.pkg.github.com/ModelingValueGroup/buildTools/com.modelingvalue.buildTools/$version/buildTools-$version.sh"
 
-  curl -s -H "Authorization: bearer $token" -L "$buildToolsUrl" -o buildTools.sh
-  . buildTools.sh
+  curl -s -H "Authorization: bearer $token" -L "$buildToolsUrl" -o buildTools-tmp.sh
+  . buildTools-tmp.sh
+  rm buildTools-tmp.sh
 }
 
 # we do not have the 'lastPackageVersion' function defined here yet. So we first load a known version here and then overwrite it with the latest:
 echo "================================================="
-includeBuildTools "$INPUT_TOKEN" "1.0.11"
+includeBuildTools "$INPUT_TOKEN" "1.0.12"
 echo "================================================="
 declare -pf listPackageVersions
 declare -pf lastPackageVersion
