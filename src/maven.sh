23d76994723f9b29f3e87b497f0b82c2848f0253
Tom Brus <tombrus@gmail.com>
2019-11-25 23:08:39 +0100

    initial push

--- src/maven.sh
+++ src/maven.sh
@@ -1,92 +1,130 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-makeAllPoms() {
-    local productUrl="$1"; shift
-    local     gitUrl="$1"; shift
-    local    version="$1"; shift
+mvn_() {
+  local token="$1"; shift
 
-    rm -f "$ARTIFACT_DIR"/*.pom
-    mkdir -p "$ARTIFACT_DIR"
-    for name in "$@"; do
-        makePomFromGavs "$productUrl" "$gitUrl" "$version" "$name" "$(findDescription "$name")" $(findAllGavsOf "$name")
-    done
-    makePomFromGavs "$productUrl" "$gitUrl" "unused" "ALL" "unused" $(findAllGavs)
+  generateMavenSettings "$USERNAME" "$token" "$GITHUB_PACKAGE_URL" >settings.xml
+  group ${DRY:-} mvn \
+    -B \
+    -s settings.xml \
+    "$@"
+  rm settings.xml
 }
-makePomFromGavs() {
-    local  productUrl="$1"; shift
-    local      gitUrl="$1"; shift
-    local     version="$1"; shift
-    local        name="$1"; shift
-    local description="$1"; shift
-    local        gavs=("$@")
+generateMavenSettings() {
+  local   username="$1"; shift
+  local   password="$1"; shift
+  local repository="$1"; shift
 
-    genDependencies() {
-        for gav in "$@"; do
-            IFS=: read g a v <<<"$gav"
-            cat <<EOF
-        <dependency>
-            <groupId>$g</groupId>
-            <artifactId>$a</artifactId>
-            <version>$v</version>
-        </dependency>
+  cat  <<EOF
+  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+    <activeProfiles>
+      <activeProfile>github</activeProfile>
+    </activeProfiles>
+
+    <profiles>
+      <profile>
+        <id>github</id>
+        <repositories>
+          <repository>
+            <id>central</id>
+            <url>https://repo1.maven.org/maven2</url>
+            <releases><enabled>true</enabled></releases>
+            <snapshots><enabled>false</enabled></snapshots>
+          </repository>
+          <repository>
+            <id>github</id>
+            <name>GitHub Apache Maven Packages</name>
+            <url>$repository</url>
+          </repository>
+        </repositories>
+      </profile>
+    </profiles>
+
+    <servers>
+      <server>
+        <id>github</id>
+        <username>$username</username>
+        <password>$password</password>
+      </server>
+    </servers>
+  </settings>
 EOF
-        done
-    }
+}
+generateAllPoms() {
+  local pom
+  for pom in *pom.xml; do
+    if [[ -f "$pom" ]]; then
+      generatePom "$pom"
+    fi
+  done
+}
+generatePom() {
+    local pom="$1"; shift
 
-    mkdir -p "$ARTIFACT_DIR"
-    local pom="$ARTIFACT_DIR/$name-SNAPSHOT.pom"
-    cat <<EOF >"$pom"
+    gave2vars "$(extractGaveFromPom "$pom")" "" ""
+
+    cat <<EOF | xmlstarlet fo >"$pom"
 <project xmlns="http://maven.apache.org/POM/4.0.0"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
-                             http://maven.apache.org/maven-v4_0_0.xsd">
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
     <modelVersion>4.0.0</modelVersion>
 
-    <groupId>$OUR_DOMAIN.$OUR_PRODUCT</groupId>
-    <artifactId>$name</artifactId>
-    <version>$([[ $version == SNAPSHOT ]] && echo "0.0.0-SNAPSHOT" || echo "$version")</version>
-    <packaging>jar</packaging>
-
-    <name>$OUR_PRODUCT $name</name>
-    <description>$description</description>
-    <url>$productUrl</url>
-
-    <licenses>
-        <license>
-            <name>GNU Lesser General Public License v3.0</name>
-            <url>https://www.gnu.org/licenses/lgpl-3.0.en.html</url>
-            <distribution>repo</distribution>
-        </license>
-    </licenses>
-
-    <scm>
-        <url>$gitUrl</url>
-    </scm>
+    <groupId>$g</groupId>
+    <artifactId>$a</artifactId>
+    <version>$v</version>
+    <packaging>$e</packaging>
 
     <dependencies>
-$([[ "${#gavs[@]}" != 0 ]] && genDependencies "${gavs[@]}")
+$(generateDependencies)
     </dependencies>
 </project>
 EOF
-    echo "    generated $pom"
 }
-findAllGavsOf() {
-    local name="$1"; shift
-
-    for iml in "$OUR_DOMAIN".$name/*.iml "$OUR_DOMAIN".$name.*/*.iml; do
-        fgrep '"Maven: ' $iml | fgrep -v 'scope="TEST"' | sed 's/.*"Maven: //;s/".*//'
+generateDependencies() {
+  local gave
+  for gave in $(findAllGaves); do
+      local g a v e
+      gave2vars "$gave" "" ""
+      echo "<dependency><groupId>$g</groupId><artifactId>$a</artifactId><version>$v</version></dependency>"
+  done
+}
+findAllGaves() {
+    local libxml
+    for libxml in .idea/libraries/*.xml; do
+      xmlstarlet sel -t -v component/library/@name <"$libxml" | sed 's/^Maven: *//'
+      echo
     done | sort -u
 }
-findAllGavs() {
-    for iml in */*.iml; do
-        fgrep '"Maven: ' $iml | sed 's/.*"Maven: //;s/".*//'
-    done | sort -u
-}
-findDescription() {
-    local name="$1"; shift
+gave2vars() {
+  local gave="$1"; shift
+  local  pom="$1"; shift
+  local file="$1"; shift
 
-    for i in "$OUR_DOMAIN".$name "$OUR_DOMAIN".$name.*; do
-        cat "$i/description" 2>/dev/null || :
-    done
+  if [[ $gave == "" && -f "$pom" ]]; then
+    gave="$(extractGaveFromPom "$pom")"
+  fi
+  if [[ $gave == "" && -f "pom.xml" ]]; then
+    gave="$(extractGaveFromPom "pom.xml")"
+  fi
+  if [[ "$gave" == "" ]]; then
+    echo "::error::can not determine group and artifact from '$gave' and '$pom'"
+    exit 55
+  fi
+  export g a v e
+  IFS=: read -r g a v e <<<"$gave"
+  if [[ $e == "" && "$file" != "" ]]; then
+    e="${file##*.}"
+  fi
+}
+extractGaveFromPom() {
+  local  pom="$1"; shift
+
+  if [[ -f "$pom" ]]; then
+    printf "%s:%s:%s:%s" \
+      "$(xmlstarlet sel -t -v /_:project/_:groupId    <"$pom" 2>/dev/null)" \
+      "$(xmlstarlet sel -t -v /_:project/_:artifactId <"$pom" 2>/dev/null)" \
+      "$(xmlstarlet sel -t -v /_:project/_:version    <"$pom" 2>/dev/null)" \
+      "$(xmlstarlet sel -t -v /_:project/_:packaging  <"$pom" 2>/dev/null)"
+  fi
 }
--- src/packages.sh
+++ src/packages.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mvn_() {
-  local token="$1"; shift
-
-  generateMavenSettings "$USERNAME" "$token" "$GITHUB_PACKAGE_URL" >settings.xml
-  group ${DRY:-} mvn \
-    -B \
-    -s settings.xml \
-    "$@"
-  rm settings.xml
-}
 downloadArtifactQuick() {
   local token="$1"; shift
   local     g="$1"; shift
@@ -55,46 +45,6 @@ uploadArtifact() {
          -DpomFile="$pom" \
              -Durl="$GITHUB_PACKAGE_URL"
 }
-generateMavenSettings() {
-  local   username="$1"; shift
-  local   password="$1"; shift
-  local repository="$1"; shift
-
-  cat  <<EOF
-  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
-    <activeProfiles>
-      <activeProfile>github</activeProfile>
-    </activeProfiles>
-
-    <profiles>
-      <profile>
-        <id>github</id>
-        <repositories>
-          <repository>
-            <id>central</id>
-            <url>https://repo1.maven.org/maven2</url>
-            <releases><enabled>true</enabled></releases>
-            <snapshots><enabled>false</enabled></snapshots>
-          </repository>
-          <repository>
-            <id>github</id>
-            <name>GitHub Apache Maven Packages</name>
-            <url>$repository</url>
-          </repository>
-        </repositories>
-      </profile>
-    </profiles>
-
-    <servers>
-      <server>
-        <id>github</id>
-        <username>$username</username>
-        <password>$password</password>
-      </server>
-    </servers>
-  </settings>
-EOF
-}
 lastPackageVersion() {
   listPackageVersions "$@" | head -1
 }
@@ -128,36 +78,4 @@ query {
 EOF
 )"
   graphqlQuery "$token" "$query" | jq -r '.data.repository.registryPackages.nodes[0].versions.nodes[].version'
-}
-gave2vars() {
-  local gave="$1"; shift
-  local  pom="$1"; shift
-  local file="$1"; shift
-
-  if [[ $gave == "" && -f "$pom" ]]; then
-    gave="$(extractGaveFromPom "$pom")"
-  fi
-  if [[ $gave == "" && -f "pom.xml" ]]; then
-    gave="$(extractGaveFromPom "pom.xml")"
-  fi
-  if [[ "$gave" == "" ]]; then
-    echo "::error::can not determine group and artifact from '$gave' and '$pom'"
-    exit 55
-  fi
-  export g a v e
-  IFS=: read -r g a v e <<<"$gave"
-  if [[ $e == "" && "$file" != "" ]]; then
-    e="${file##*.}"
-  fi
-}
-extractGaveFromPom() {
-  local  pom="$1"; shift
-
-  if [[ -f "$pom" ]]; then
-    printf "%s:%s:%s:%s" \
-      "$(xmlstarlet sel -t -v /_:project/_:groupId    <"$pom")" \
-      "$(xmlstarlet sel -t -v /_:project/_:artifactId <"$pom")" \
-      "$(xmlstarlet sel -t -v /_:project/_:version    <"$pom")" \
-      "$(xmlstarlet sel -t -v /_:project/_:packaging  <"$pom")"
-  fi
 }
