1abe3f56065eaa6b09b4fc262b7353d62c152bbe
Tom Brus <tombrus@gmail.com>
2019-11-27 23:27:05 +0100

    try without docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
+##########################################################################################################################
+extraPackages=(xmlstarlet jq maven)
+      ourUser="ModelingValueGroup"
+      product="buildTools"
+      groupId="com.modelingvalue"
+   artifactId="$product"
+
+##########################################################################################################################
 echo "::group::install extra packages"
-sudo apt-get install -y xmlstarlet jq maven
+sudo apt-get install -y "${extraPackages[@]}"
 echo "::endgroup::"
 
 includeBuildTools() {
   local   token="$1"; shift
   local version="$1"; shift
 
-  local buildToolsUrl="https://maven.pkg.github.com/ModelingValueGroup/buildTools/com.modelingvalue.buildTools/$version/buildTools-$version.sh"
+  local url="https://maven.pkg.github.com/$ourUser/$product/$groupId.$artifactId/$version/$artifactId-$version.sh"
 
-  curl -s -H "Authorization: bearer $token" -L "$buildToolsUrl" -o buildTools.sh
-  . buildTools.sh
+  curl -s -H "Authorization: bearer $token" -L "$url" -o $artifactId.sh
+  . $artifactId.sh
 }
 
+##########################################################################################################################
 # we do not have the 'lastPackageVersion' function defined here yet
-# so we first load a known version here and then overwrite it with the latest:
+# so we first load a known version here....
 includeBuildTools "$INPUT_TOKEN" "1.0.19"
-includeBuildTools "$INPUT_TOKEN" "$(lastPackageVersion "$INPUT_TOKEN" "ModelingValueGroup/buildTools" "com.modelingvalue:buildTools" "")"
+
+# ...and then overwrite it with the latest:
+latest="$(lastPackageVersion "$INPUT_TOKEN" "$ourUser/$product" "$groupId:$artifactId" "")"
+includeBuildTools "$INPUT_TOKEN" "$latest"
+echo "INFO: installed $artifactId version $latest"
