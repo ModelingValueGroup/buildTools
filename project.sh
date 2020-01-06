cbcc569359cbb14201a6548f318703f5778f024d
Tom Brus <tombrus@gmail.com>
2020-01-06 22:57:43 +0100

    use base64

--- project.sh
+++ project.sh
@@ -14,7 +14,7 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 artifacts=(
-  "org.modelingvalue   buildTools           1.2.2        jar j--"
+  "org.modelingvalue   buildTools           1.2.3        jar j--"
 )
 dependencies=(
 )
