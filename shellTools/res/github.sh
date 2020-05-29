169e110d4f71a9e46621244084e58c441bdee115
Tom Brus <tombrus@gmail.com>
2020-05-29 15:05:18 +0200

    add tag setting utils

--- shellTools/res/github.sh
+++ shellTools/res/github.sh
@@ -37,22 +37,6 @@ pushBackToGithub() {
         echo "no changes need to be pushed back to github"
     fi
 }
-setVersionTagIfMaster() {
-    if [[ "${GITHUB_REF##*/}" == master ]]; then
-        . <(catProjectSh)
-        # shellcheck disable=SC2154
-        local tagName="v$version"
-        if [[ "$(git tag | fgrep -Fx "$tagName")" != "" ]]; then
-            echo "::error::tag for this version ($tagName) already exists"
-        else
-            echo "setting tag $tagName"
-            git tag "$tagName"
-            git push origin "$tagName"
-        fi
-    else
-        echo "ok: not on master"
-    fi
-}
 errorIfMasterAndVersionTagExists() {
     if [[ "${GITHUB_REF##*/}" == master ]]; then
         . <(catProjectSh)
@@ -62,6 +46,27 @@ errorIfMasterAndVersionTagExists() {
             echo "::error::tag for this version ($tagName) already set, can not build on master"
         else
             echo "ok: not such tag"
+        fi
+    else
+        echo "ok: not on master"
+    fi
+}
+setVersionTagIfMaster() {
+    local token="$1"; shift
+    local email="$1"; shift
+
+    if [[ "${GITHUB_REF##*/}" == master ]]; then
+        . <(catProjectSh)
+        # shellcheck disable=SC2154
+        local tagName="v$version"
+        if [[ "$(git tag | fgrep -Fx "$tagName")" != "" ]]; then
+            echo "::error::tag for this version ($tagName) already exists"
+        else
+            echo "setting tag $tagName"
+            git config user.email "$email"
+            git config user.name "$GITHUB_ACTOR"
+            git tag "$tagName"
+            git push "$(getGithubSecureUrl "$token")" "$tagName"
         fi
     else
         echo "ok: not on master"
