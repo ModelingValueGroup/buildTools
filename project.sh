df3dc6ba4654c597aec4f233478d8c4daa47484e
Tom Brus <tombrus@gmail.com>
2020-05-29 15:14:36 +0200

    add tag setting utils

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="2.2.1"
+version="2.2.2"
 artifacts=(
     "org.modelingvalue   buildTools   $version   jar  j--"
 )
