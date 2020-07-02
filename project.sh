d80be3e0ddc31ab7bac10e1470e0c913c9838653
Tom Brus <tombrus@gmail.com>
2020-07-02 22:03:52 +0200

    add release publishing to action script

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="2.2.8"
+version="2.2.9"
 artifacts=(
     "org.modelingvalue   buildTools   $version   jar  j--"
 )
