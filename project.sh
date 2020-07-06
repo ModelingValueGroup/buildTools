eb21d830c77cea0a1e47e8c0c4c602479eb4ccc7
Tom Brus <tombrus@gmail.com>
2020-07-06 13:01:55 +0200

    avoid error msg on secrets file

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="2.3.4"
+version="2.3.5"
 artifacts=(
     "org.modelingvalue   buildTools   $version   jar  j--"
 )
