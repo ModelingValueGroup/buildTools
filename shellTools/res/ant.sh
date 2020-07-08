ba31b575ba563f009a5a9be312aced30a39d54cd
Tom Brus <tombrus@gmail.com>
2020-07-09 00:13:41 +0200

    cater for junit5

--- shellTools/res/ant.sh
+++ shellTools/res/ant.sh
@@ -18,10 +18,7 @@ set -euo pipefail
 
 installJunitlauncher() {
     if [[ ! -f "$ANT_HOME/lib/ant-junitlauncher.jar" ]]; then
-        echo "ANT_HOME=$ANT_HOME"
-        ls -la "$ANT_HOME"
-        ls -la "$ANT_HOME/lib"
+        echo "downloading missing ant-junitlauncher.jar..."
         downloadArtifact '' 'org.apache.ant' 'ant-junitlauncher' '1.10.8' 'jar' "$ANT_HOME/lib"
-        jar tvf "$ANT_HOME/lib/ant-junitlauncher.jar"
     fi
 }
