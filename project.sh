c78b8317567783a28d1b2a95aea12350725fe3ae
Tom Brus <tombrus@gmail.com>
2020-07-08 17:03:41 +0200

    cater for junit5

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="2.3.6"
+version="2.3.7"
 artifacts=(
     "org.modelingvalue   buildTools   $version   jar  j--"
 )
