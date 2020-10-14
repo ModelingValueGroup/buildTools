b998a4c13e1c0bae022930705d054bd52c62599c
Tom Brus <tombrus@gmail.com>
2020-10-15 00:09:10 +0200

    publish buildtools instead of buildTools

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="3.0.1"
+version="3.0.2"
 artifacts=(
     "org.modelingvalue   buildtools   $version   jar  j--"
 )
