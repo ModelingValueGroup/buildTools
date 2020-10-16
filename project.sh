1cd5af424c2d32968b89dc637769eb4403c420e8
Tom Brus <tombrus@gmail.com>
2020-10-16 13:29:53 +0200

    check the meme

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="3.1.2"
+version="3.1.3"
 artifacts=(
     "org.modelingvalue   buildtools   $version   jar  j--"
 )
