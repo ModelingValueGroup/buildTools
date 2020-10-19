a6f18b1f80f20e07260fda7711240b27337c0ef0
Tom Brus <tombrus@gmail.com>
2020-10-19 10:20:28 +0200

    bunp version for meme

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="3.3.3"
+version="3.3.4"
 artifacts=(
     "org.modelingvalue   buildtools   $version   jar  j--"
 )
