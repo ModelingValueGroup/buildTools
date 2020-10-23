7fb7d4a8161d8d40c54e410238ebcf6e5e129fc3
automation <automation@modelingvalue.com>
2020-10-23 11:07:24 +0200

    refactor GITHUB_TOKEN handling and more

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="3.4.0"
+version="3.4.1"
 artifacts=(
     "org.modelingvalue   buildtools   $version   jar  j--"
 )
