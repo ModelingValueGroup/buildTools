57710301cc72699bf77fd2a423990f4f8475fce4
automation <automation@modelingvalue.com>
2020-11-25 17:37:16 +0000

    automatic reformat by actions

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version='4.0.1'
+version='4.0.2'
 artifacts=(
     "org.modelingvalue   buildtools   $version   jar  j--"
 )
